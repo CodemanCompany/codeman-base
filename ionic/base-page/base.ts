2d78f016d2ac78cd750e5d3927ac27ab018853c6
Gustavo Martínez <gustavo@codeman.company>
2018-05-13 17:45:52 -0500

    Progress Darwin

---
+++ ionic/base-page/base.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+// import { Dialogs } from '@ionic-native/dialogs';
+// import { APIService } from '../../app/service/api.service';
+
+@Component( {
+	selector: 'page-base',
+	templateUrl: 'base.html'
+} )
+export class BasePage {
+	constructor(
+		// private apiService: APIService,
+		// private dialogs: Dialogs,
+		private navCtrl: NavController
+	) {}	// end constructor
+
+	private ionViewDidLoad(): void {
+	}	// end method
+
+	private action(): void {
+	}	// end method
+}	// enc class
